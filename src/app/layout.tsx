847588381d574f3fd7a29da061832e232ae0e7c6
sahilshah668 <149790769+sahil-breach@users.noreply.github.com>
2025-08-29 23:31:55 +0530

    Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,17 @@ export default function RootLayout({
             gtag('config', 'G-0RP4G3S7LP');
           `}
         </Script>
+        
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t1fkiqndov");
+          `}
+        </Script>
         {children}
       </body>
     </html>
